f4f9b03514c6e0bd53d73ec0a57ba089be75af94
Sky1sBloo <ultimate8gaming@gmail.com>
2025-05-30 09:36:50 +0800

    Added prototype functions on delivery system

---
+++ src/controllers/deliveryController.js
@@ -0,0 +1,25 @@
+/**
+ * Addes a new delivery
+ * @param req.body
+ * {
+ *      product_name: string,
+ *      sender: string,
+ *      recipient: string,
+ *      destination: string,
+ *      source: string,
+ *      date_shipped: date,
+ *      deadline: timestamp,
+ *      status: delivery_status
+ * }
+ */
+export const addDelivery = (req, res, next) => {
+}
+
+export const editDeliveryInfo = (req, res, next) => {
+}
+
+export const deleteDelivery = (req, res, next) => {
+}
+
+export const suggestDeliveryRoute = (req, res, next) => {
+}
