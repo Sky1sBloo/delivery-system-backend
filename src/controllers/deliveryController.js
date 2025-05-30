55b8f2f1b238fd8ece103ba49c18d8f503356b79
Sky1sBloo <ultimate8gaming@gmail.com>
2025-05-30 13:52:15 +0800

    Added delivery method definition

--- src/controllers/deliveryController.js
+++ src/controllers/deliveryController.js
@@ -1,3 +1,4 @@
+import supabase from '../database.js';
 /**
  * Addes a new delivery
  * @param req.body
@@ -12,9 +13,39 @@
  *      status: delivery_status
  * }
  */
-export const addDelivery = (req, res, next) => {
+export const addDelivery = async (req, res, next) => {
+    const requiredParams = [req.body.product_name, req.body.recipient, req.body.destination, req.body.source, req.body.deadline];
+    try {
+        for (const value of requiredParams) {
+            if (value == undefined) {
+                return res.status(400).json({ message: 'Missing body parameters' });
+            }
+        }
+        const { error } = supabase.from('delivery').insert(req.body);
+        if (error) {
+            throw new Error(error);
+        }
+
+        return res.status(201).json({message: 'Created new delivery'});
+    } catch (error) {
+        next(error);
+    }
 }
 
+/**
+ * Addes a new delivery
+ * @param req.body
+ * {
+ *      product_name: string,
+ *      sender: string,
+ *      recipient: string,
+ *      destination: string,
+ *      source: string,
+ *      date_shipped: date,
+ *      deadline: timestamp,
+ *      status: delivery_status
+ * }
+ */
 export const editDeliveryInfo = (req, res, next) => {
 }
 
