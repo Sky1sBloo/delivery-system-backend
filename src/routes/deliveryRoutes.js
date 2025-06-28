5eaa946eebe362dea70b2ec1185d1f8c88964eb5
Sky1sBloo <ultimate8gaming@gmail.com>
2025-06-28 14:12:42 +0800

    Added authentication on delivery routes

--- src/routes/deliveryRoutes.js
+++ src/routes/deliveryRoutes.js
@@ -4,12 +4,12 @@ import { isDelivery, isManagement } from '../middlewares/authentication.js';
 
 const router = Router();
 
-router.get('/all', retrieveDeliveries);
+router.get('/all', isManagement, retrieveDeliveries);
 router.get('/', isDelivery, retrieveOwnedDeliveries);
 router.post('/', isManagement, addDelivery);
 router.put('/:id', isManagement, editDeliveryInfo);
-router.delete('/:id', deleteDelivery);
-router.get('/route', getDeliveryRoute);
-router.get('/items', suggestDeliveryItems);
+router.delete('/:id', isManagement, deleteDelivery);
+router.get('/route', isDelivery, getDeliveryRoute);
+router.get('/items', isManagement, suggestDeliveryItems);
 
 export default router;
